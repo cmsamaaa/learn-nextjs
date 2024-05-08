02298ee401f93ac86a91ca9674b318096fb61409
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-05-08 23:05:07 +0800

    section 5

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,25 +1,12 @@
-import { redirect } from 'next/navigation';
-import { db } from '@/db';
+'use client';
+
+import { useFormState } from 'react-dom';
+import * as actions from '@/actions';
 
 export default function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    'use server';
-
-    const title = formData.get('title') as string;
-    const code = formData.get('code') as string;
-
-    await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-
-    redirect('/');
-  }
-
+  const [formState, action] = useFormState(actions.createSnippet, { message: '' });
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className='m-3 font-bold'>Create a snippet</h3>
       <div className='flex flex-col gap-4'>
         <div className='flex gap-4'>
@@ -49,6 +36,12 @@ export default function SnippetCreatePage() {
             id='code'
           />
         </div>
+
+        {formState.message ? (
+          <div className='my-2 rounded border border-red-400 bg-red-200 p-2'>
+            {formState.message}
+          </div>
+        ) : null}
 
         <button
           type='submit'
